bc740cbf2ab08afb14b84af70e3e62fe8516c34e
umayr-dev <abuqodir0107@gmail.com>
2025-05-12 22:59:16 +0500

    commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import './Login.css'
+import './login.css'
 
 function Login() {
   const [username, setUsername] = useState('')
